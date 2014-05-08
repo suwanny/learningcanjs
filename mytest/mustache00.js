63d7a28a15054f4e2f728970de52ecf87c589788
Soo Hwan Park <suwanny@gmail.com>
2014-05-08 08:32:45 -0700

    add canjs as submodule

--- mytest/mustache00.js
+++
@@ -1,7 +0,0 @@
-/*
-
-Mustache is a logic-less templating languages [which provide live binding when used with Observes. CanJS's Mustache implementation supports both normal Mustache templates as well as the Handlebars extensions, allowing you to easily reuse templates that you've already written.
-
-
-
-*/
